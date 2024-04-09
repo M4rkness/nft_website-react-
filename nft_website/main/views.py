43e619e3b382ab348a3bf52717cae47e11c1e4ba
Bismark_x <xxwoll4nxx@gmail.com>
2024-04-09 18:18:36 +0500

    add urls

--- nft_website/main/views.py
+++ nft_website/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.generics import CreateAPIView, DestroyAPIView, UpdateAPIView, ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+""""""
+class IndexAPIView(APIView):
+    def get(self, request):
+        return Response(dict())
